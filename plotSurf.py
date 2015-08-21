3d6864bd438c2bc4b01b16447713928e4089f75e
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-08-21 15:48:24 -0600

    plotting ok

--- plotSurf.py
+++ plotSurf.py
@@ -11,21 +11,36 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
+import os
 
 ''' set color codes in seaborn '''
 sns.set_color_codes()
 
+base_directory='/home/rvalenzuela/SURFACE'
+print base_directory
+usr_case = raw_input('\nIndicate case number (i.e. 1): ')
+case='case'+usr_case.zfill(2)
+casedir=base_directory+'/'+case
+out=os.listdir(casedir)
+out.sort()
+files=[]
+for f in out:
+	if f[-3:]=='met': 
+		print f
+		files.append(f)
+usr_location = raw_input('\nIndicate location (i.e. bby): ')
+file_met=[]
+for f in files:
+	if f[:3]==usr_location:
+		file_met.append(casedir+'/'+f)
 
-# file_met=['/home/rvalenzuela/SURFACE/case03/bby01023.met',
-# 			'/home/rvalenzuela/SURFACE/case03/bby01024.met']
-
-file_met=['/home/rvalenzuela/SURFACE/case03/czc01023.met',
-			'/home/rvalenzuela/SURFACE/case03/czc01024.met']
-
-name_col=['press','temp','rh','wspd','wdir','precip','mixr']
-
-# index_col=[3,6,9,10,12,17,26] # bby
-index_col=[3,4,5,6,8,13,23] # czc
+name_field=['press','temp','rh','wspd','wdir','precip','mixr']
+if usr_location=='bby':
+	index_field=[3,6,9,10,12,17,26] # bby
+	locname='Bodega Bay'
+elif usr_location=='czc':
+	index_field=[3,4,5,6,8,13,23] # czc
+	locname='Cazadero'
 
 def main():
 
@@ -56,10 +71,9 @@ def parse_dataframe(file_met):
 	dates=raw_dates.apply(lambda x: datetime.strptime(x, dates_fmt))
 
 	''' make meteo df, assign datetime index, and name columns '''
-	meteo=dframe.ix[:,index_col]
+	meteo=dframe.ix[:,index_field]
 	meteo.index=dates
-	meteo.columns=name_col
-
+	meteo.columns=name_field
 
 	''' make field with hourly acum precip '''
 	hour=pd.TimeGrouper('H')
@@ -69,7 +83,7 @@ def parse_dataframe(file_met):
 	''' assign metadata '''
 	units = {'press':'mb', 'temp':'C', 'rh':'%', 'wspd':'m s-1', 'wdir':'deg', 'precip':'mm', 'mixr': 'g kg-1'}
 	agl = {'press':'NaN', 'temp':'10 m', 'rh':'10 m', 'wspd':'NaN', 'wdir':'NaN', 'precip':'NaN', 'mixr': 'NaN'}
-	for n in name_col:
+	for n in name_field:
 		meteo[n].units=units[n]
 		meteo[n].agl=agl[n]
 		meteo[n].nan=-9999.999
@@ -105,12 +119,14 @@ def make_plot(meteo):
 	ax[3].set_ylabel('Rain rate [mm h-1]',color='b',fontsize=labsize)	
 	ax[3].xaxis.set_major_formatter(mdates.DateFormatter('%d-%H'))
 	ax[3].set_xlabel(r'$\Leftarrow$'+' Time (UTC)')
+	
+	l1='Surface meteorology at '+ locname
+	l2='\nStart time: '+x[0].strftime('%Y-%m-%d %H:%M')+' UTC'
+	l3='\nEnd time: '+x[-1].strftime('%Y-%m-%d %H:%M')+' UTC'
+	fig.suptitle(l1+l2+l3,y=0.98)
+
 	plt.draw()
 
-	# fig,ax = plt.subplots()
-	# ax.plot(val,'-o')
-	# ax.invert_xaxis()
-	# plt.draw()
 
 def add_second_yaxis(ax,x,y):
 	axt=ax.twinx()
@@ -120,8 +136,6 @@ def add_second_yaxis(ax,x,y):
 
 
 
-
-
-
 main()
 plt.show()
+# plt.show(block=False)
