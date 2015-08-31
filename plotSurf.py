1c33a3298e00f7530f0a6a815be1861305dbc241
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-08-31 16:24:18 -0600

    parsing function moved to new module Meteoframes

--- plotSurf.py
+++ plotSurf.py
@@ -5,13 +5,12 @@
 	August, 2015
 """
 
-from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
-import Thermodyn as thermo
+import Meteoframes as mf
 import os
 
 ''' set color codes in seaborn '''
@@ -49,7 +48,7 @@ def main():
 
 	df=[]
 	for f in file_met:
-		df.append(parse_dataframe(f))
+		df.append(mf.parse_surface(f,index_field,name_field,locelevation))
 
 	if len(df)>1:
 		meteo=pd.concat(df)
@@ -58,53 +57,6 @@ def main():
 
 	make_meteo(meteo)
 	make_thermo(meteo)
-
-def parse_dataframe(file_met):
-
-	dates_col=[0,1,2]
-	dates_fmt='%Y %j %H%M'
-
-	''' read the csv file '''
-	dframe = pd.read_csv(file_met,header=None)
-
-	''' parse date columns into a single date col '''
-	raw_dates=dframe.ix[:,dates_col]
-	raw_dates.columns=['Y','j','HHMM']
-	raw_dates['HHMM'] = raw_dates['HHMM'].apply(lambda x:'{0:0>4}'.format(x))
-	raw_dates=raw_dates.apply(lambda x: '%s %s %s' % (x['Y'],x['j'],x['HHMM']), axis=1)
-	dates=raw_dates.apply(lambda x: datetime.strptime(x, dates_fmt))
-
-	''' make meteo df, assign datetime index, and name columns '''
-	meteo=dframe.ix[:,index_field]
-	meteo.index=dates
-	meteo.columns=name_field
-
-	''' make field with hourly acum precip '''
-	hour=pd.TimeGrouper('H')
-	preciph = meteo.precip.groupby(hour).sum()
-	meteo = meteo.join(preciph, how='outer', rsuffix='h')
-
-	''' add thermodynamics '''
-	theta = thermo.theta(C=meteo.temp,hPa=meteo.press)
-	thetaeq = thermo.theta_equiv(C=meteo.temp,hPa=meteo.press)
-	meteo.loc[:,'theta'] = pd.Series(theta,index=meteo.index)	
-	meteo.loc[:,'thetaeq'] = pd.Series(thetaeq,index=meteo.index)	
-
-	''' add sea level pressure '''
-	Tv = thermo.virtual_temperature(C=meteo.temp,mixing_ratio=meteo.mixr/1000.)
-	slp = thermo.sea_level_press(K=Tv+273.15, Pa=meteo.press*100, m=locelevation)
-	meteo.loc[:,'sea_levp']=slp
-
-	''' assign metadata (prototype, not really used) '''
-	units = {'press':'mb', 'temp':'C', 'rh':'%', 'wspd':'m s-1', 'wdir':'deg', 'precip':'mm', 'mixr': 'g kg-1'}
-	agl = {'press':'NaN', 'temp':'10 m', 'rh':'10 m', 'wspd':'NaN', 'wdir':'NaN', 'precip':'NaN', 'mixr': 'NaN'}
-	for n in name_field:
-		meteo[n].units=units[n]
-		meteo[n].agl=agl[n]
-		meteo[n].nan=-9999.999
-		meteo[n].sampling_freq='1 minute'	
-
-	return meteo
 
 def make_meteo(meteo):
 
@@ -194,5 +146,5 @@ def add_second_yaxis(ax,x,y):
 
 
 main()
-# plt.show()
-plt.show(block=False)
+plt.show()
+# plt.show(block=False)
