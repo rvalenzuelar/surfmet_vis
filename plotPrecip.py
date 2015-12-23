efce8361c2a3f7d0181a34a8e93b010ac28c573a
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-12-23 12:33:57 -0700

    added significant dates

--- plotPrecip.py
+++ plotPrecip.py
@@ -15,53 +15,59 @@ base_directory='/home/rvalenzuela/SURFACE'
 # base_directory='/Users/raulv/Documents/SURFACE'
 usr_case=[] # set within get_index_field
 
-def main():
-
-	# global usr_case
-	# plot_compare_accum(period=True)
-
-
-	# fig,ax=plt.subplots(2,1,figsize=(7,11))
-	# plot_compare_sum(ax=ax[0],minutes=30, period=True)
-	# plot_regression(ax=ax[1],minutes=30, period=True)
-	# plt.show(block=False)
-
-
-
-	# ppsurf=PdfPages('surf_precipsum_multipage.pdf')
-	# ncases=range(1,15)
-	# # ncases=[1,2,3]
-	# for c in ncases:
-	# 	fig,ax=plt.subplots()
-	# 	plot_compare_sum(ax=ax,usr_case=str(c), ylim=[0,14], minutes=30, period=True)
-	# 	ppsurf.savefig()
-	# 	plt.close('all')
-	# ppsurf.close()
-
-	# ppsurf=PdfPages('surf_precipreg_multipage.pdf')
-	# ncases=range(1,15)
-	# # ncases=[1,2,3]
-	# for c in ncases:
-	# 	fig,ax=plt.subplots()
-	# 	plot_regression(ax=ax,usr_case=str(c), minutes=30, period=True)
-	# 	ppsurf.savefig()
-	# 	plt.close('all')
-	# ppsurf.close()
-
-
-def plot_compare_accum(**kwargs):
+def main(plot=True):
 
 	global usr_case
 
-	bby, czd = get_data()
+	if plot:
+
+		# fig,ax=plt.subplots(2,1,figsize=(6, 8))
+		# plot_compare_accum(ax=ax, period=False)
+		# plt.show(block=False)
+
+		# fig,ax=plt.subplots(2,1,figsize=(7,9))
+		# plot_compare_sum(ax=ax[0],minutes=30, period=None)
+		# plot_regression(ax=ax[1],minutes=30, period=None)
+		# plt.show(block=False)
+
+		# fig,ax=plt.subplots(2,1,figsize=(7,9))
+		# plot_compare_sum(ax=ax[0],minutes=30, period='significant')
+		# plot_regression(ax=ax[1],minutes=30, period='significant')				
+		# plt.show(block=False)
+
+		ppsurf=PdfPages('surf_precipsum_significant_60min.pdf')
+		ncases=range(1,15)
+		# ncases=[1,2,3]
+		for c in ncases:
+			fig,ax=plt.subplots()
+			plot_compare_sum(ax=ax,usr_case=str(c), ylim=[0,22], minutes=60, period='significant')
+			ppsurf.savefig()
+			plt.close('all')
+		ppsurf.close()
+
+		# ppsurf=PdfPages('surf_precipreg_multipage.pdf')
+		# ncases=range(1,15)
+		# # ncases=[1,2,3]
+		# for c in ncases:
+		# 	fig,ax=plt.subplots()
+		# 	plot_regression(ax=ax,usr_case=str(c), minutes=30, period=True)
+		# 	ppsurf.savefig()
+		# 	plt.close('all')
+		# ppsurf.close()
+
+
+def plot_compare_accum(ax=None, usr_case=None,**kwargs):
+
+
+	bby, czd, usr_case = get_data(usr_case)
 	period=kwargs['period']
 
 	if period:
 		period = get_request_dates(usr_case)
-		ini = period['ini']
-		end = period['end']
-		inix = bby.index.get_loc(datetime(ini[0],ini[1],ini[2],ini[3],0))
-		endx = bby.index.get_loc(datetime(end[0],end[1],end[2],end[3],0))		
+		ini = datetime(*(period['ini']+[0]))
+		end = datetime(*(period['end']+[0]))
+		inix = bby.index.get_loc(ini)
+		endx = bby.index.get_loc(end)		
 	else:
 		inix=0
 		endx=-1
@@ -72,25 +78,21 @@ def plot_compare_accum(**kwargs):
 	reg = get_regression(bby_paccum.values,czd_paccum.values)
 	print 'm={:3.2f}, Rsq={:3.2f}, n={:d}'.format(reg[0],reg[1],reg[2])
 
-	fig,ax=plt.subplots()
-	ax.plot(bby_paccum,label='BBY')
-	ax.plot(czd_paccum,label='CZD')
-	ax.grid()
-	plt.legend()
-	plt.suptitle('Accumulated precip case '+usr_case)
-	plt.draw()
+	ax[0].plot(bby_paccum,label='BBY')
+	ax[0].plot(czd_paccum,label='CZD')
+	ax[0].grid()
+	ax[0].legend(loc=0)
+	ax[0].set_title('Accumulated precip case '+usr_case)
 
-	fig,ax=plt.subplots()
-	ax.scatter(bby_paccum, czd_paccum, color='blue', s=30, edgecolor='None',alpha=0.9)
-	ax.plot(range(len(bby_paccum)),color='black',linestyle='--',linewidth=3)
+	ax[1].scatter(bby_paccum, czd_paccum, color='blue', s=30, edgecolor='None',alpha=0.9)
+	ax[1].plot(range(len(bby_paccum)),color='black',linestyle='--',linewidth=3)
 	mmax = np.amax([np.max(bby_paccum), np.max(czd_paccum)])
-	ax.set_xlim([0,mmax])
-	ax.set_ylim([0,mmax])
-	ax.set_xlabel('bby')
-	ax.set_ylabel('czd')
-	ax.grid()
-	plt.suptitle('case '+usr_case)
-	plt.draw()
+	ax[1].set_xlim([0,mmax])
+	ax[1].set_ylim([0,mmax])
+	ax[1].set_xlabel('bby')
+	ax[1].set_ylabel('czd')
+	ax[1].grid()
+	ax[1].set_title('Scatter plot precip case '+usr_case)
 
 
 def plot_compare_sum(ax=None,usr_case=None,ylim=None,**kwargs):
@@ -117,17 +119,21 @@ def plot_compare_sum(ax=None,usr_case=None,ylim=None,**kwargs):
 	ax.set_ylabel('Rain rate [mm freq-1]',color='k',fontsize=labsize)	
 	ax.grid()
 	if period:
-		period = get_request_dates(usr_case)
-		ini = period['ini']
-		end = period['end']
-		inix = bbyg.index.get_loc(datetime(ini[0],ini[1],ini[2],ini[3],0))
-		endx = bbyg.index.get_loc(datetime(end[0],end[1],end[2],end[3],0))
+		if period=='5h':
+			period = request_dates_5h(usr_case)
+		elif period=='significant':
+			period = request_dates_significant(usr_case)
+		ini = datetime(*(period['ini']+[0]))
+		end = datetime(*(period['end']+[0]))
+		inix = bbyg.index.get_loc(ini)
+		endx = bbyg.index.get_loc(end)
 		ax.set_xlim([xg[inix]-timed, xg[endx]-timed])
 		y=bbyg[inix:endx+1].values
 		x=czdg[inix:endx+1].values
 		reg = get_regression(x,y)
 		bbyg=bbyg.ix[inix:endx+1]
 		czdg=czdg.ix[inix:endx+1]
+
 	ax.invert_xaxis()
 	if ylim is not None:
 		ax.set_ylim([ylim[0], ylim[1]])
@@ -139,14 +145,13 @@ def plot_compare_sum(ax=None,usr_case=None,ylim=None,**kwargs):
 	plt.subplots_adjust(bottom=0.15, top=0.95, left=0.1,right=0.95)
 
 
-def plot_regression(ax=None,usr_case=None,**kwargs):
+def plot_regression(ax=None,usr_case=None,period=None,**kwargs):
 
 	if ax is None:
 		ax=plt.gca()
 
 	bby,czd,usr_case = get_data(usr_case)
 	minutes=kwargs['minutes']
-	period=kwargs['period']
 	timeg=pd.TimeGrouper(str(minutes)+'T')
 	bbyg = bby.precip.groupby(timeg).sum()
 	czdg = czd.precip.groupby(timeg).sum()
@@ -156,23 +161,37 @@ def plot_regression(ax=None,usr_case=None,**kwargs):
 
 	xg=bbyg.index+timed
 	if period:
-		period = get_request_dates(usr_case)
-		ini = period['ini']
-		end = period['end']
-		inix = bbyg.index.get_loc(datetime(ini[0],ini[1],ini[2],ini[3],0))
-		endx = bbyg.index.get_loc(datetime(end[0],end[1],end[2],end[3],0))
+		if period=='5h':
+			period = request_dates_5h(usr_case)
+		elif period=='significant':
+			period = request_dates_significant(usr_case)			
+		ini = datetime(*(period['ini']+[0]))
+		end = datetime(*(period['end']+[0]))
+		inix = bbyg.index.get_loc(ini)
+		endx = bbyg.index.get_loc(end)
 		y=bbyg[inix:endx].values
 		x=czdg[inix:endx].values
 		reg = get_regression(x,y)
 		bbyg=bbyg.ix[inix:endx]
 		czdg=czdg.ix[inix:endx]
+		begdate=ini
+		enddate=end
+	else:
+		y=bbyg.values
+		x=czdg.values
+		reg = get_regression(x,y)
+		begdate=bby.index[0]
+		enddate=bby.index[-1]
 
 	ax.scatter(bbyg,czdg,color='blue')
 	bbyg_max=np.max(bbyg)
 	czdg_max=np.max(czdg)
 	regtext='m={:3.2f}, Rsq={:3.2f}, n={:d}'.format(reg[0],reg[1],reg[2])
-	str_times=xg[inix].strftime('%dT%H:00 - ')+xg[endx].strftime('%dT%H:00 UTC')
-	ax.text(0.3,0.95,str_times, size=15, transform=ax.transAxes)
+	str_beg=begdate.strftime('Beg: %dT%H:%M UTC')
+	str_end=enddate.strftime('\nEnd: %dT%H:%M UTC')
+	str_freq='\nFreq: '+str(minutes)+' minutes'
+	str_times=str_beg+str_end+str_freq
+	ax.text(0.1,0.9,str_times, size=15, verticalalignment='top', transform=ax.transAxes)
 	ax.text(0.4,0.1,regtext,weight='bold', size=18, transform=ax.transAxes)
 	vmax=int(np.ceil(np.maximum(bbyg_max,czdg_max)))
 	ax.plot(range(-1,15),range(-1,15),color='black')
@@ -266,7 +285,7 @@ def get_data(usr_case=None):
 	index_field, name_field = get_index_field(usr_case)
 	dfBBY=[]
 	dfCZD=[]
-	period = get_request_dates(usr_case)
+	# period = get_request_dates(usr_case)
 	for f in file_met:
 		loc=f[-12:-9]
 		if loc=='bby':
@@ -286,9 +305,11 @@ def get_data(usr_case=None):
 
 	return meteoBBY, meteoCZD, usr_case
 
-def get_request_dates(usr_case):
+def request_dates_5h(usr_case):
 
-
+	''' 	these dates select continuos periods of precip
+		in 5 hour
+	'''
 	reqdates={ '1': {'ini':[1998,1,18,15],'end':[1998,1,18,20]},
 				'2': {'ini':[1998,1,26,4],'end':[1998,1,26,9]},
 				'3': {'ini':[2001,1,23,21],'end':[2001,1,24,2]},
@@ -303,6 +324,30 @@ def get_request_dates(usr_case):
 				'12': {'ini':[2004,2,2,12],'end':[2004,2,2,17]},
 				'13': {'ini':[2004,2,17,14],'end':[2004,2,17,19]},
 				'14': {'ini':[2004,2,25,8],'end':[2004,2,25,13]}
+				}	
+
+	return reqdates[usr_case]
+
+def request_dates_significant(usr_case):
+
+	''' 	these dates try to avoid discontinuity when possible 
+		and periods of no rain at the beginning and end 
+		of the time series 
+	'''
+	reqdates={ '1': {'ini':[1998,1,18,15],'end':[1998,1,19,0]},
+				'2': {'ini':[1998,1,26,0],'end':[1998,1,27,5]},
+				'3': {'ini':[2001,1,23,12],'end':[2001,1,24,6]},
+				'4': {'ini':[2001,1,25,9],'end':[2001,1,26,23]},
+				'5': {'ini':[2001,2,9,8],'end':[2001,2,10,14]},
+				'6': {'ini':[2001,2,11,2],'end':[2001,2,11,13]},
+				'7': {'ini':[2001,2,17,11],'end':[2001,2,17,23]},
+				'8': {'ini':[2003,1,12,10],'end':[2003,1,14,13]},
+				'9': {'ini':[2003,1,21,4],'end':[2003,1,23,8]},
+				'10': {'ini':[2003,2,15,20],'end':[2003,2,16,10]},
+				'11': {'ini':[2004,1,9,14],'end':[2004,1,9,23]},
+				'12': {'ini':[2004,2,2,9],'end':[2004,2,2,23]},
+				'13': {'ini':[2004,2,16,6],'end':[2004,2,18,6]},
+				'14': {'ini':[2004,2,25,6],'end':[2004,2,25,19]}
 				}	
 
 	return reqdates[usr_case]
